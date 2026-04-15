5eeb7989e78ef781d48ba41c52166f909eab739d
siritell <siri.tell@hotmail.com>
2026-04-15 18:33:40 +0200

    - Fixed broken server package.json dependencies (express, cors, mongodb) - Reinstalled backend node_modules to restore API functionality - Fixed image rendering in React - Updated image src paths to work with /JavaScript-React routing

---
+++ Assignment 2/project/client/src/ReviewCard.jsx
@@ -0,0 +1,30 @@
+import "./ReviewCard.css";
+
+function ReviewCard({ review, onEdit, onDelete }) {
+  return (
+    <div className="review-card">
+      <img
+        src={`/JavaScript-React/${review.image}`}
+        alt={review.name}
+        className="review-image"
+      />
+      <div className="review-content">
+        <h3>{review.name}</h3>
+        <p>{review.address}</p>
+        <p>{review.text}</p>
+        <p>
+          <b>Standout's:</b>
+          <br />
+          {review.standouts.map((item, idx) => (
+            <span key={idx}>
+              {item}
+              <br />
+            </span>
+          ))}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ReviewCard;
